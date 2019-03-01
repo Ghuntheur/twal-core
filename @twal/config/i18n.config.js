035611f0dbaacee850024c060f1c3c219fbf9831
Lila <lilagazeau@gmail.com>
2019-03-01 17:12:01 +0100

    build: added ./ before each link

--- @twal/config/i18n.config.js
+++ @twal/config/i18n.config.js
@@ -16,7 +16,7 @@ i18n
     defaultNS: 'common',
     fallbackNS: ['common'],
     backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json'
+      loadPath: './locales/{{lng}}/{{ns}}.json'
     }
   });
 
