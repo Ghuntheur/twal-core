5b0565e62d39f8933c7b984423ddb2b09afcc357
florian <zobeleflorian@gmail.com>
2019-03-02 02:03:53 +0100

    style: use hooks instead of class

--- @twal/components/Twal.jsx
+++ @twal/components/Twal.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState, useRef } from 'react';
 import propTypes from 'prop-types';
 import IdleTimer from 'react-idle-timer';
 
@@ -9,33 +9,24 @@ import '@twal/config/i18n.config';
 
 import '@twal/styles/index';
 
-class Twal extends React.Component {
-  constructor() {
-    super();
-    this.idleTimer = null;
+const Twal = ({ children }) => {
+  const idleTimer = useRef(null);
+  const [isIdle, setIdle] = useState(false);
 
-    this.state = {
-      isIdle: false
-    };
-  }
-
-  render() {
-    const { isIdle } = this.state;
-    return (
-      <Suspense fallback={<Loader />}>
-        <IdleTimer
-          ref={el => (this.idleTimer = el)}
-          element={document}
-          timeout={1000 * 3}
-          onIdle={() => this.setState({ isIdle: true })}
-          onAction={() => this.setState({ isIdle: false })}
-        >
-          {isIdle && <ScreenSaver />}
-        </IdleTimer>
-      </Suspense>
-    );
-  }
-}
+  return (
+    <Suspense fallback={<Loader />}>
+      <IdleTimer
+        ref={idleTimer}
+        element={document}
+        timeout={1000 * 3}
+        onIdle={() => setIdle(true)}
+        onAction={() => setIdle(false)}
+      >
+        {isIdle && <ScreenSaver />}
+      </IdleTimer>
+    </Suspense>
+  );
+};
 
 Twal.propTypes = {
   children: propTypes.element.isRequired
