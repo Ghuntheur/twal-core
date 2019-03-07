f2164b1017e4869425988580ff6ca233f101d3bd
florian <zobeleflorian@gmail.com>
2019-03-07 15:13:14 +0100

    feat: add / route and Home default basic component

--- @twal/components/core/MainContent.jsx
+++ @twal/components/core/MainContent.jsx
@@ -1,27 +1,48 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 
 import { Page } from '@twal';
+import Home from '@twal/components/ui/Home';
 
 import twalConfig from '@root/twal.config';
 
 const MainContent = () => {
   const { routes } = twalConfig;
 
-  return routes.map(route => {
-    const Component = require(`@root/src/pages/${route.component}`).default;
-    return (
+  const defaultRoute = routes && (routes.find(route => route.default) || routes[0]);
+
+  return (
+    <>
       <Route
-        key={route.component}
-        path={`/${route.component}`}
-        component={() => (
-          <Page>
-            <Component />
-          </Page>
-        )}
+        exact
+        path="/"
+        render={() =>
+          defaultRoute ? (
+            <Redirect to={`/${defaultRoute.component.toLowerCase()}`} />
+          ) : (
+            <Page>
+              <Home />
+            </Page>
+          )
+        }
       />
-    );
-  });
+      {routes &&
+        routes.map(route => {
+          const Component = require(`@root/src/pages/${route.component}`).default;
+          return (
+            <Route
+              key={route.component}
+              path={`/${route.component}`}
+              component={() => (
+                <Page>
+                  <Component />
+                </Page>
+              )}
+            />
+          );
+        })}
+    </>
+  );
 };
 
 export default MainContent;
