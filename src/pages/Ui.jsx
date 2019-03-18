6397eb0709b2668d36513dd75170e479129ac6c9
sudo-cho <noisiv@outlook.fr>
2019-03-18 16:03:53 +0100

    feat: plugin videoFullscreen

--- src/pages/Ui.jsx
+++ src/pages/Ui.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import IconButton from '@twal/components/ui/IconButton';
 import Carousel from '@twal/components/plugins/carousel/Carousel';
+import VideoFullscreen from '@twal/components/plugins/videoFullscreen/VideoFullscreen';
 
 const carouselProps = {
   dots: true,
@@ -10,11 +11,21 @@ const carouselProps = {
   slidesToScroll: 1
 };
 
+const videoFullScreenProps = {
+  playsInline: true,
+  fluid: false,
+  width: 640,
+  poster: '../assets/img/poster.jpg',
+  src: 'https://media.w3.org/2010/05/sintel/trailer_hd.mp4'
+};
+
 const Ui = () => (
   <section>
     <h1>UI kit default</h1>
+
     <IconButton icon="cancel" className="btn-icon" />
     <IconButton icon="cog" className="btn-icon" />
+
     <Carousel settings={carouselProps}>
       <div>
         <h3>1</h3>
@@ -26,6 +37,8 @@ const Ui = () => (
         <h3>3</h3>
       </div>
     </Carousel>
+
+    <VideoFullscreen settings={videoFullScreenProps} />
   </section>
 );
 
