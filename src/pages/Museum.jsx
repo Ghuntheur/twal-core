a60bf96126a487fe64012a0c394902b2383c0458
florian <zobeleflorian@gmail.com>
2019-03-22 01:04:27 +0100

    fix: fix re render on link click
    wip

--- src/pages/Museum.jsx
+++ src/pages/Museum.jsx
@@ -12,13 +12,30 @@ import SideMenu from '@twal/components/nav/SideMenu';
 const Museum = ({ t }) => {
   const [Routes, links] = createSubRoutes('/museum', BaseComponent, 20);
 
-  const timeline = () => (
-    <Scrollable>
+  const Timeline = () => (
+    <Scrollable style={{ width: 60 }}>
       <SideMenu links={links} type="timeline" name="Menu exemple" />
     </Scrollable>
   );
 
-  return <OneFourthLayout Small={timeline} Big={Routes} />;
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+      <Scrollable style={{ background: 'red', width: '300px', height: '100vh' }}>
+        <ul>
+          {links.map(link => (
+            <li style={{ margin: '40px 0' }} key={link.key}>
+              {link}
+            </li>
+          ))}
+        </ul>
+      </Scrollable>
+      <div style={{ background: 'blue', width: 500 }}>
+        <Routes />
+      </div>
+    </div>
+  );
+
+  // return <OneFourthLayout Small={timeline} Big={Routes} />;
 };
 
 Museum.propTypes = {
