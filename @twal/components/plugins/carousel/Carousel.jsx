43ac7793798b02b4af65023256c9cdf61c2d3cac
Flroian <zobeleflorian@gmail.com>
2019-03-22 09:57:17 +0100

    refactor: plugins code harmonization

--- @twal/components/plugins/carousel/Carousel.jsx
+++ @twal/components/plugins/carousel/Carousel.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Slider from 'react-slick';
 
-import './carousel.scss';
+import '@twal/styles/vendors/carousel.scss';
 
 /*
  * Component Carousel is based on react-slick
  * All props and methods can be found here: https://react-slick.neostack.com/docs/api/
  */
-export default class Carousel extends React.Component {
-  render() {
-    return <Slider {...this.props.settings}>{this.props.children}</Slider>;
-  }
-}
+const Carousel = ({ children, ...rest }) => <Slider {...rest}>{children}</Slider>;
+
+Carousel.propTypes = {
+  children: PropTypes.node.isRequired
+};
--- @twal/components/plugins/videoFullscreen/VideoFullscreen.jsx
+++ @twal/components/plugins/videoFullscreen/VideoFullscreen.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Player } from 'video-react';
 
-import './videoFullscreen.scss';
+import '@twal/styles/vendors/videoFullscreen.scss';
 
 /*
  * Component VideoFullScreen is based on Video-React
  * All props and methods can be found here: https://video-react.js.org/components/player/
  */
-export default class VideoFullScreen extends React.Component {
-  render() {
-    return <Player {...this.props.settings} />;
-  }
-}
+const VideoFullScreen = ({ ...rest }) => <Player {...rest} />;
+
+export default VideoFullScreen;
