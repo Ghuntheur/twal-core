0a8f4b75d6675de7802c524b22e2e2f099f6d484
Flroian <zobeleflorian@gmail.com>
2019-03-25 19:21:09 +0100

    feat: add utils functions

--- @twal/functions/index.js
+++ @twal/functions/index.js
@@ -1,5 +1,11 @@
+import twalConfig from '@root/twal.config';
 import i18n from '@twal/config/i18n.config';
 
 export const getLanguages = () => i18n.languages;
 export const getCurrentLanguage = () => i18n.language;
 export const getI18nConfig = () => i18n;
+export const getConfiguration = () => twalConfig;
+export const getRoutes = () =>
+  twalConfig.routing.routes.map(
+    route => `/${(route.path ? route.path.replace(/^\//, '') : route.component).toLowerCase()}`
+  );
